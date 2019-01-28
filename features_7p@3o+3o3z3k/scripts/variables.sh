9d5239287893be6697b1dee6d4ca2143ba4eb757
kyehyukahn <alwayswinner@erickyehyukahn.local>
2019-01-28 18:21:08 +0900

    plus 3 orders

---
+++ features_7p@3o+3o3z3k/scripts/variables.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SYS_CHANNEL='testchainid'
+ORDERER_GENSIS=orderer.genesis.block
+ORDERER_GENSIS_PROFILE=ThreeOrgsOrdererGenesis
+APP_CHANNEL="businesschannel"
+APP_CHANNEL_PROFILE=ThreeOrgsChannel
+UPDATE_ANCHOR_ORG1_TX=Org1MSPanchors.tx
+UPDATE_ANCHOR_ORG2_TX=Org2MSPanchors.tx
+UPDATE_ANCHOR_ORG3_TX=Org3MSPanchors.tx
+CHANNEL_ARTIFACTS=channel-artifacts
+ORG1MSP="Org1MSP"
+ORG2MSP="Org2MSP"
+ORG3MSP="Org3MSP"
